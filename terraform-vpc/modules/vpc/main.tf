5060fe057cce89e59dd666b8c266cef203e9562e
Milan Obrenovic <mmobrenovic@gmail.com>
2023-10-08 21:17:14 +0200

    By default, subnets are set as private IP addresses, this is now fixed so that they are public on launch

--- terraform-vpc/modules/vpc/main.tf
+++ terraform-vpc/modules/vpc/main.tf
@@ -10,10 +10,11 @@ resource "aws_vpc" "my_vpc" {
 
 # 2 Subnets
 resource "aws_subnet" "subnets" {
-	vpc_id            = aws_vpc.my_vpc.id
-	cidr_block        = var.subnet_cidr[count.index]
-	count             = length(var.subnet_cidr)
-	availability_zone = data.aws_availability_zones.available.names[count.index]
+	vpc_id                  = aws_vpc.my_vpc.id
+	cidr_block              = var.subnet_cidr[count.index]
+	count                   = length(var.subnet_cidr)
+	availability_zone       = data.aws_availability_zones.available.names[count.index]
+	map_public_ip_on_launch = true
 
 	tags = {
 		Name = var.subnet_names[count.index]
@@ -21,6 +22,7 @@ resource "aws_subnet" "subnets" {
 }
 
 # Internet Gateway
+
 
 # Route table
 
