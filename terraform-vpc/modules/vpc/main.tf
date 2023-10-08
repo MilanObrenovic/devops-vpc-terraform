17f15f10caa2ca40a5e82441def4651b6abb653a
Milan Obrenovic <mmobrenovic@gmail.com>
2023-10-08 20:25:28 +0200

    Added 2 subnets

--- terraform-vpc/modules/vpc/main.tf
+++ terraform-vpc/modules/vpc/main.tf
@@ -2,12 +2,23 @@
 resource "aws_vpc" "my_vpc" {
 	cidr_block       = var.vpc_cidr
 	instance_tenancy = "default"
-	tags             = {
+
+	tags = {
 		"Name" = "my_vpc"
 	}
 }
 
-# Subnets
+# 2 Subnets
+resource "aws_subnet" "subnets" {
+	vpc_id            = aws_vpc.my_vpc.id
+	cidr_block        = var.subnet_cidr[count.index]
+	count             = length(var.subnet_cidr)
+	availability_zone = data.aws_availability_zones.available.names[count.index]
+
+	tags = {
+		Name = var.subnet_names[count.index]
+	}
+}
 
 # Internet Gateway
 
--- terraform-vpc/variables.tf
+++ terraform-vpc/variables.tf
@@ -2,3 +2,8 @@ variable "vpc_cidr" {
 	description = "VPC CIDR range."
 	type        = string
 }
+
+variable "subnet_cidr" {
+	description = "Subnet CIDRs."
+	type        = list(string)
+}
