23e32494ce2868439bc59d91f23a5b9294461b94
Milan Obrenovic <mmobrenovic@gmail.com>
2023-10-08 19:21:36 +0200

    Added s3 backend in main.tf

--- terraform-vpc/main.tf
+++ terraform-vpc/main.tf
@@ -1,8 +1,14 @@
 terraform {
 	required_providers {
 		aws = {
-			source = "hashicorp/aws"
+			source  = "hashicorp/aws"
 			version = "5.20.0"
 		}
+	}
+
+	backend "s3" {
+		bucket = "terraform-remote-backend-s3"
+		key    = "dev/terraform.tfstate"
+		region = "eu-central-1"
 	}
 }
