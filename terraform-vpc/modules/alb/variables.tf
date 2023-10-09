eaab8657b707a2901f765b176d19348c839c50a3
Milan Obrenovic <mmobrenovic@gmail.com>
2023-10-09 14:44:01 +0200

    Added aws_lb_listener, aws_lb_target_group and aws_lb_target_group_attachment

--- terraform-vpc/modules/alb/variables.tf
+++ terraform-vpc/modules/alb/variables.tf
@@ -7,3 +7,13 @@ variable "subnets" {
 	description = "Subnets for ALB."
 	type        = list(string)
 }
+
+variable "vpc_id" {
+	description = "VPC ID for ALB."
+	type        = string
+}
+
+variable "instances" {
+	description = "Instance ID for Target Group Attachment."
+	type        = list(string)
+}
