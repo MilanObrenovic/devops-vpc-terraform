6f1aceaba596abd75056577560aa75b02f071a5a
Milan Obrenovic <mmobrenovic@gmail.com>
2023-10-09 11:44:39 +0200

    Added a name tag for aws_route_table

--- terraform-vpc/modules/vpc/main.tf
+++ terraform-vpc/modules/vpc/main.tf
@@ -39,6 +39,10 @@ resource "aws_route_table" "rt" {
 		cidr_block = "0.0.0.0/0"
 		gateway_id = aws_internet_gateway.igw.id
 	}
+
+	tags = {
+		"Name" = "MyRouteTable"
+	}
 }
 
 # Route table association
